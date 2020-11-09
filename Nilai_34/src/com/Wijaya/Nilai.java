16101544009c22e507861ffa5733c2859c128990
WijayaGanda <64698425+WijayaGanda@users.noreply.github.com>
2020-11-09 10:25:25 +0700

    Wijaya (34)

---
+++ Nilai_34/src/com/Wijaya/Nilai.java
@@ -0,0 +1,12 @@
+package com.Wijaya;
+
+public class Nilai {
+
+    public int getnilaiPengetahuan (int pengetahuan){
+        return pengetahuan;
+    }
+
+    public int getnilaiKeterampilan (int keterampilan){
+        return keterampilan;
+    }
+}
